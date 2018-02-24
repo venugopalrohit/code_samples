d3bfe80c957b88a04496fe2b5d7646e1c871c63c
Rohit Venugopal <rohit_v4@yahoo.com>
2018-02-23 22:34:00 -0500

    Added comments, completed sorting code for hash

--- word_count.py
+++ word_count.py
@@ -1,9 +1,14 @@
 # Program to find the top "n" words in a given input text
-# Aim is to limit memory usage
+# Aim is to limit memory usage - though I am reading the entire file into memory :(
+
+# The idea here is to read the file into a string, and then use a regex to weed out small words
+# The regex returns a list of strings that I will iterate over and store in a hash, along with frequency
+# Finally get a sorted representation of the hash, using the operator module and sorted method
 
 import re
+import operator
 
-input_file = open("complex_word_count_input.txt", "r")
+input_file = open("word_count_input.txt", "r")
 #Read the whole file into memory and convert to an array of strings
 file_data = input_file.read()
 
@@ -21,5 +26,9 @@ for word in file_data:
 
 print(words_dict)
 
+# Finally, get a sorted version of the dictionary based on values
+# Using the operator module which I don't fully understand
+sorted_keys = sorted(words_dict.items(), key=operator.itemgetter(1), reverse=True)
+print(sorted_keys)
 
 input_file.close()
